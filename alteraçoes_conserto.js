b65b62fcb2cee3d9b2b0515c34296bff55677b36
Henrique Venancio Dos Santos <hqsantos1@gmail.com>
2021-12-15 23:24:31 -0300

    meu trabalho

---
+++ alteraçoes_conserto.js
@@ -0,0 +1,42 @@
+
+const consertos = require('./consertos');
+console.log `Aparelhos prontos e seus valores`
+consertos.forEach(conserto => {if
+    
+    (conserto.pronto === true) {totalValor = 0;
+
+    conserto.valores.forEach(valor => {totalValor += valor})
+
+    console.log(`Aparelho: ${conserto.aparelho} 
+    itensConsertados:
+    * ${conserto.itensConsertados[0]},
+    * ${conserto.itensConsertados[1]},    
+    * ${conserto.itensConsertados[2]},
+    valor total: R$ ${totalValor}
+    `)
+
+
+
+}
+
+})
+
+// aparelhos prontos e não prontos
+const prontos = consertos.filter((entrega)=> {return entrega.pronto})
+
+const nãoProntos = consertos.filter(conserto => conserto.pronto === false);
+
+console.log(nãoProntos);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ consertos.js
@@ -0,0 +1,140 @@
+ let consertos = [ { aparelho: 'Samsung A10',
+
+itensConsertados: [ 'tela', 'bateria', 'teclado' ],
+
+valores: [ 10, 100, 25 ], pronto: true} 
+
+
+, { aparelho: 'Galaxy s7',
+
+itensConsertados: [ 'carcaça', 'tampa traseira',' camera traseira' ],
+
+valores: [ 250, 120, 250 ], pronto: false}
+
+
+, { aparelho: 'Iphone 5s',
+
+itensConsertados: [ 'bateria', 'conector de carga','tela' ],
+
+valores: [ 150, 70, 300 ], pronto: true}
+
+
+
+, { aparelho: 'Iphone 8',
+
+itensConsertados: [ 'tela','entrada usb','camera frontal' ],
+
+valores: [ 500, 100, 300 ], pronto: true}
+
+
+
+, { aparelho: 'Moto X play',
+
+itensConsertados: [ 'bateria', 'conector de carga', 'conector p3'],
+
+valores: [ 200, 60, 40 ], pronto: false}
+
+
+
+
+
+, { aparelho: 'Alcatel',
+
+itensConsertados: [ 'bateria', 'conector de carga', 'botão power' ],
+
+valores: [ 170, 35, 50 ], pronto: true}
+
+
+
+
+
+, { aparelho: 'Redmi note 9s',
+
+itensConsertados: [ 'tela', 'camera frontal', 'lateral' ],
+
+valores: [ 400, 250, 300 ], pronto: false}
+
+
+
+
+
+, { aparelho: 'Iphone x',
+
+itensConsertados: [ 'NFC', 'tampa traseira', 'carcaça' ],
+
+valores: [ 250, 120, 530 ], pronto: true}
+
+
+
+
+, { aparelho: 'Galaxy J7 metal',
+
+itensConsertados: [ 'aro lateral', 'auto falante','plug p2' ],
+
+valores: [ 70, 65, 50 ], pronto: true}
+
+
+
+
+, { aparelho: 'Xperia z ultra',
+
+itensConsertados: [ 'camera traseira', 'tela', 'camera frontal'],
+
+valores: [ 150, 500,250 ], pronto: false}
+
+
+
+
+, { aparelho: 'Iphone 11 pro max',
+
+itensConsertados: [ 'tela', 'traseira de vidro', 'lateral' ],
+
+valores: [ 1000, 550, 400 ], pronto: true}
+
+
+, { aparelho: 'Iphone 7',
+
+itensConsertados: [ 'bateria', 'carcaça', 'traseira' ],
+
+valores: [ 300, 200, 300 ], pronto: true}
+
+
+, { aparelho: 'Iphone 8',
+
+itensConsertados: [ 'camera frontal','camera traseira','frontal' ],
+
+valores: [ 300, 250,100 ], pronto: false}
+
+
+, { aparelho: 'Galaxy note 9 plus',
+
+itensConsertados: [ 'tela', 'caneta', 'traseira de vidro' ],
+
+valores: [500, 100, 250 ], pronto: false}
+
+
+, { aparelho: 'Nokia luminia 520',
+
+itensConsertados: [ 'bateria','conector de carga','carcaça' ],
+
+valores: [ 250, 120, 55 ],   pronto: true},]
+
+
+
+
+
+
+module.exports = consertos;
+
+
+
+
+
+
+
+
+ 
+
+
+
+ 
